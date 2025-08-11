b64a25863826d6da43d6fd06c15c7f83c557bec0
Arsalan Bilal <134631939+arsalanbilal@users.noreply.github.com>
2025-08-12 02:20:37 +0530

    health.app.py

--- health.app.py
+++ health.app.py
@@ -21,10 +21,10 @@ st.info("Adjust the input parameters in the sidebar to see the prediction in rea
 # Sidebar inputs
 with st.sidebar:
     st.header('📊 Input Features')
-    Recency = st.slider('Recency', 0, 26)
-    Frequency = st.slider('Frequency', 1, 12)
-    Monetary = st.slider('Monetary', 250, 3000)
-    Time = st.slider('Time', 2, 99)
+    Recency = st.slider('Recency', 0., 26.)
+    Frequency = st.slider('Frequency', 1., 12.)
+    Monetary = st.slider('Monetary', 250., 3000.)
+    Time = st.slider('Time', 2,, 99.)
 
 # Predict button
 if st.button("🚀 Predict"):
